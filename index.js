f2a7934920839c6382b110ace2b8f2fdaddc251e
AlinaPulya <alvasamoilova@gmail.com>
2017-08-14 13:25:35 +0300

    Add array of object with books

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+var library = [{
+  name: "The Paper",
+  cover: "",
+  description: ""
+  }, {
+  name: "Even in Darkness",
+  cover: "",
+  description: ""
+  }, {
+  name: "Flashpoint",
+  cover: "",
+  description: ""
+  }];
+
 $('#FirstModal').modal('show');
 $('#SecondModal').modal('show');
 $('#ThirddModal').modal('show');
+
+console.log(document);
+console.log(document.documentElement);
+console.log(document.body);
+console.log(document.head);
