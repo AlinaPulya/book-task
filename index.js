395360576b8d8342bc20dacfe417ba7e292c000f
AlinaPulya <alvasamoilova@gmail.com>
2017-08-14 13:32:34 +0300

    Add full array of objects

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 var library = [{
   name: "The Paper",
-  cover: "",
-  description: ""
+  cover: "img/book_1.bmp",
+  description: "In this bone-chilling tale of terror from Shamus"
+  +"Award-winning thriller writer Lynn Hightower, a devoted mother"
+  +"must confront the paranormal forces that have cursed her family..."
   }, {
   name: "Even in Darkness",
-  cover: "",
-  description: ""
+  cover: "img/book_2.bmp",
+  description: "Joy Miller, once a famed TV evangelist, retired years"
+  +"before when two tragedies struck her family: the first leading her"
+  +"husband to suicide the second leaving her son dead and his wife and"
+  +"daughter estranged from her..."
   }, {
   name: "Flashpoint",
-  cover: "",
-  description: ""
+  cover: "img/book_3.bmp",
+  description: "A New York Times Notable Book: Cincinnati homicide"
+  +"detective Sonora Blair hunts a serial killer who's playing with"
+  +"fire in Shamus Award—winning author Lynn Hightower's chilling thriller."
   }];
 
 $('#FirstModal').modal('show');
