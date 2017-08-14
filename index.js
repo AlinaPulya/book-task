2b7aeaf10ef63b707103c4b284d423a194fa2b4f
AlinaPulya <alvasamoilova@gmail.com>
2017-08-14 20:26:03 +0300

    Add new key-word in object data_target, for which would be open modal window

--- index.js
+++ index.js
@@ -3,31 +3,34 @@ var library = [{
   cover: "img/book_1.bmp",
   description: "In this bone-chilling tale of terror from Shamus"
   +"Award-winning thriller writer Lynn Hightower, a devoted mother"
-  +"must confront the paranormal forces that have cursed her family..."
+  +"must confront the paranormal forces that have cursed her family...",
+  data_target: "#FirstModal"
   }, {
   name: "Even in Darkness",
   cover: "img/book_2.bmp",
   description: "Joy Miller, once a famed TV evangelist, retired years"
   +"before when two tragedies struck her family: the first leading her"
   +"husband to suicide the second leaving her son dead and his wife and"
-  +"daughter estranged from her..."
+  +"daughter estranged from her...",
+  data_target: "#SecondModal"
   }, {
   name: "Flashpoint",
   cover: "img/book_3.bmp",
   description: "A New York Times Notable Book: Cincinnati homicide"
   +"detective Sonora Blair hunts a serial killer who's playing with"
-  +"fire in Shamus Award—winning author Lynn Hightower's chilling thriller."
+  +"fire in Shamus Award—winning author Lynn Hightower's chilling thriller.",
+  data_target: "#ThirdModal"
   }];
 
 function createbook(library){
   library.forEach(function(elem){
-    var parentElem = document.getElementById('content-test');
+    var parentElem = document.getElementById('appear_book');
     var elementbook = document.createElement('div');
     elementbook.className = "col-md-4";
     elementbook.innerHTML = '<img  class="img-rounded" src='+elem.cover+' alt="The Paper Book"><h3>'+elem.name+'</h3><p align="justify">'
   +elem.description+'</p> <!-- Button trigger modal -->'
   +'<button type="button" class="btn btn-primary" data-toggle="modal"'
-  +'data-target="#FirstModal">View more </button>';
+  +'data-target="'+elem.data_target+'">View more </button>';
   parentElem.appendChild(elementbook);
 });
 }
